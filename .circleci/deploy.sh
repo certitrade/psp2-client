9d1bd0e9738b1d1baad228cf658301d92c94473c
Simon Mika <simon@mika.se>
2019-02-06 10:54:23 +0100

    Removed superflous comments.

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,11 +4,6 @@ git config --global user.email circleci@circleci
 git config --global user.name CircleCI
 git remote --v
 
-# Bump the version
 npm version patch
-
-# Push the new tag to Github
 git push --tags
-
-# Publish to NPM
 npm publish
